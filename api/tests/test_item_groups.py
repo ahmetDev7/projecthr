0686434bcd247fb7a768bb8f0c81cb9f5151d21d
AlperenBaytimur <alperen.baytimur@gmail.com>
2024-10-21 11:52:24 +0200

    Chore: test_client & test_item 2 extra happy paths, test_item_group created

---
+++ api/tests/test_item_groups.py
@@ -0,0 +1,137 @@
+import unittest
+from httpx import Client
+from httpx import codes
+
+# 7 happy paths
+# 9 edge cases
+
+
+class Testitem_groups(unittest.TestCase):
+
+    def setUp(self):
+        self.reader_token = "f6g7h8i9j0"
+        self.client = Client(base_url="http://localhost:3000/api/v1/",
+                             headers={"Content-Type": "application/json", "API_KEY": "a1b2c3d4e5"})
+
+    def test_get_all_readertoken(self):
+        self.client.headers["API_KEY"] = self.reader_token
+        response = self.client.get("/item_groups")
+
+        self.assertEqual(response.status_code, codes.OK)
+    
+    def test_get_single_readertoken(self):
+        self.client.headers["API_KEY"] = self.reader_token
+        response = self.client.get("/item_groups/8")
+
+        self.assertEqual(response.status_code, codes.OK)
+    
+    def test_unauthorized_get_all(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        response = self.client.get("/item_groups")
+
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_get_single_incorrect_unauthorized(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        response = self.client.get("/item_groups/-90")
+
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_get_single_unauthorized(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        response = self.client.get("/item_groups/8")
+
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_create_unauthorized(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        new_client = {
+            "id": 101,
+            "name": "Electronics",
+            "description": "I did it mensen",
+            "created_at": "1998-05-15 19:52:53",
+            "updated_at": "2000-11-20 08:37:56"
+
+        }
+
+        response = self.client.post("/item_groups", json=new_client)
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_update_unauthorized(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        updated_client = {
+            "id": 3,
+            "name": "Osman Smith",
+            "description": "I did it mensen",
+            "created_at": "1998-05-15 19:52:53",
+            "updated_at": "2000-11-20 08:37:56"
+        }
+
+        response = self.client.put("/item_groups/3", json=updated_client)
+
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_delete_unauthorized(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        response = self.client.delete("/item_groups/90")
+
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_delete_incorrect_id_unauthorized(self):
+        self.client.headers["API_KEY"] = "wrong_key"
+        response = self.client.delete("/item_groups/-10")
+
+        self.assertEqual(response.status_code, codes.UNAUTHORIZED)
+
+    def test_get_all(self):
+        response = self.client.get("/item_groups")
+
+        self.assertEqual(response.status_code, codes.OK)
+        self.assertTrue(len(response.json()) > 0)
+
+    def test_get_single(self):
+        response = self.client.get("/item_groups/8")
+
+        self.assertEqual(response.status_code, codes.OK)
+        self.assertIn("id", response.json())
+
+    def test_get_incorrect_id(self):
+        response = self.client.get("/item_groups/-1")
+
+        self.assertEqual(response.status_code, codes.NOT_FOUND)
+
+    def test_create_item_group(self):
+        new_item_group = {
+            "id": 100,
+            "name": "Electronics",
+            "description": "Osman en Alperen",
+            "created_at": "1998-05-15 19:52:53",
+            "updated_at": "2000-11-20 08:37:56"
+
+        }
+
+        response = self.client.post("/item_groups", json=new_item_group)
+        self.assertEqual(response.status_code, 201)
+
+    def test_update_item_groups(self):
+        updated_client = {
+            "id": 0,
+            "name": "Electronics",
+            "description": "I did it mensen",
+            "created_at": "1998-05-15 19:52:53",
+            "updated_at": "2000-11-20 08:37:56"
+        }
+
+        response = self.client.put("/item_groups/0", json=updated_client)
+
+        self.assertEqual(response.status_code, codes.OK)
+
+    def test_delete_client(self):
+        response = self.client.delete("/item_groups/90")
+
+        self.assertEqual(response.status_code, codes.OK)
+
+    def test_delete_incorrect_id(self):
+        response = self.client.delete("/item_groups/-10")
+
+        self.assertEqual(response.status_code, codes.NOT_FOUND)

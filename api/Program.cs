721becb603345fac0b05ba8b3690556db78c72ef
Sharif-C <sharifali.786@hotmail.com>
2024-10-22 19:26:28 +0200

    build: initial postgres connection

--- api/Program.cs
+++ api/Program.cs
@@ -1,11 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 string jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), "jsondata/locations.json");
 builder.Services.AddSingleton(new LocationsProvider(jsonFilePath));

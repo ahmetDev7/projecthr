c580e8e571a2c3ea4bae6502a0d7f94d8e3eb7a9
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-10-23 14:58:13 +0200

    feature(Program.cs): Added UseAuthorization & MapControllers & AddControllers

--- api/Program.cs
+++ api/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -48,6 +50,9 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 var app = builder.Build();
+
+app.UseAuthorization();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello world 🚀");
 

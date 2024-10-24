07eb635e4b856af4a1ebd82d57ee37ab5fee4c40
Sharif-C <sharifali.786@hotmail.com>
2024-10-24 16:09:51 +0200

    build(models)!: based on db erd and migrate to postgres db
    BREAKING CHANGE: Multiple model  attributes changed old models no longer valid

--- api/contexts/AppDbContext.cs
+++ api/contexts/AppDbContext.cs
@@ -3,4 +3,9 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> db) : base(db) { }
+
+    public DbSet<Warehouse> Warehouses {get; set;}
+    public DbSet<Location> Locations {get; set;}
+    public DbSet<Address> Addresses {get; set;}
+    public DbSet<Contact> Contacts {get; set;}
 } 

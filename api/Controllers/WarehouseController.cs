29cba4c76c44d0fd25afe7f73f5984d3a0a8e81a
AlperenBaytimur <alperen.baytimur@gmail.com>
2024-10-24 13:33:28 +0200

    feature: Warehouse get by id

---
+++ api/Controllers/WarehouseController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+ 
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class WarehousesController : ControllerBase
+{
+
+   private readonly WarehouseProvider _warehouseProvider;
+
+   public WarehousesController(WarehouseProvider warehouseProvider)
+   {
+         _warehouseProvider = warehouseProvider;
+   } 
+
+    [HttpGet("{id}")]  // Dit specificeert dat {id} een routeparameter is
+    public ActionResult<Warehouse> GetByID(int id)
+    {
+        try
+        {
+            var warehouse = _warehouseProvider.GetByID(id);
+            return Ok(warehouse);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return NotFound(new { Message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { Message = ex.Message });
+        }
+    }
+}

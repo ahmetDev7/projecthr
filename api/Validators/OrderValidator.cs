8a8bba725b51170ff4cea1a9c545c74efaff51b0
sidi <sidimo2001@gmail.com>
2024-11-16 17:44:41 +0100

    feature(order): warehouse id validator added

--- api/Validators/OrderValidator.cs
+++ api/Validators/OrderValidator.cs
@@ -1,9 +1,10 @@
+using System.Data;
 using FluentValidation;
 using Model;
 
 public class OrderValidator : AbstractValidator<Order>
 {
-    public OrderValidator()
+    public OrderValidator(AppDbContext db)
     {
         RuleFor(Order => Order.OrderDate)
             .NotNull().WithMessage("order date required")
@@ -11,13 +12,16 @@ public class OrderValidator : AbstractValidator<Order>
         RuleFor(Order => Order.OrderStatus)
             .NotNull().WithMessage("order status required")
             .NotEmpty().WithMessage("order status cannot be empty.");
-        //TODO:clientsId
         RuleFor(Order => Order.WarehouseId)
             .NotNull().WithMessage("warehouse id required")
-            .NotEmpty().WithMessage("warehouse id cannot be empty.");
+            .NotEmpty().WithMessage("warehouse id cannot be empty.")
+            .Custom((warehouseId, context ) => {
+                if (!db.Warehouses.Any(w => w.Id == warehouseId))
+                {
+                    context.AddFailure("warehouse_id", "The provided warehouse_id does not exist");
+                }
+            });
+        //TODO:clientsId
 
     }
-
-
-
 }

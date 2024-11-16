f7c594754ef7dcd3b0f1f853b9718580473eba85
sidi <sidimo2001@gmail.com>
2024-11-16 18:35:55 +0100

    fix(order): validator changed to snake_case

--- api/Validators/OrderValidator.cs
+++ api/Validators/OrderValidator.cs
@@ -7,14 +7,14 @@ public class OrderValidator : AbstractValidator<Order>
     public OrderValidator(AppDbContext db)
     {
         RuleFor(Order => Order.OrderDate)
-            .NotNull().WithMessage("order date required")
-            .NotEmpty().WithMessage("order date cannot be empty.");
+            .NotNull().WithMessage("order_date required")
+            .NotEmpty().WithMessage("order_date cannot be empty.");
         RuleFor(Order => Order.OrderStatus)
-            .NotNull().WithMessage("order status required")
-            .NotEmpty().WithMessage("order status cannot be empty.");
+            .NotNull().WithMessage("order_status required")
+            .NotEmpty().WithMessage("order_status cannot be empty.");
         RuleFor(Order => Order.WarehouseId)
-            .NotNull().WithMessage("warehouse id required")
-            .NotEmpty().WithMessage("warehouse id cannot be empty.")
+            .NotNull().WithMessage("warehouse_id required")
+            .NotEmpty().WithMessage("warehouse_id cannot be empty.")
             .Custom((warehouseId, context ) => {
                 if (!db.Warehouses.Any(w => w.Id == warehouseId))
                 {

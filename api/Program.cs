f8e3a900c9795224e0565c54d2dba164166e3ae2
sidi <sidimo2001@gmail.com>
2024-11-16 16:04:15 +0100

    feature(order): add provider and validator to the program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -99,10 +99,12 @@ builder.Services.AddTransient<WarehouseProvider>();
 builder.Services.AddTransient<ItemsProvider>();
 builder.Services.AddTransient<LocationsProvider>();
 builder.Services.AddTransient<ItemGroupProvider>();
+builder.Services.AddTransient<OrderProvider>();
 
 builder.Services.AddScoped<IValidator<Location>, LocationValidator>();
 builder.Services.AddScoped<IValidator<Item>, ItemValidator>();
 builder.Services.AddScoped<IValidator<ItemGroup>, ItemGroupValidator>();
+builder.Services.AddScoped<IValidator<Order>, OrderValidator>();
 
 builder.Services.AddControllers();
 

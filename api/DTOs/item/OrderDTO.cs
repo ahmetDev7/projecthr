c84311bc3e54ea9b1f90ad9824d1711c605e38b2
sidi <sidimo2001@gmail.com>
2024-11-16 17:37:10 +0100

    chore(order): dto reqest snake_case

--- api/DTOs/item/OrderDTO.cs
+++ api/DTOs/item/OrderDTO.cs
@@ -6,38 +6,40 @@ namespace DTO.Order
     [ApiExplorerSettings(IgnoreApi = true)]
     public class OrderRequest : BaseDTO
     {
-        [JsonPropertyName("orderdate")]
+        [JsonPropertyName("order_date")]
         public DateTime? OrderDate { get; set; }
-        [JsonPropertyName("requestdate")]
+        [JsonPropertyName("request_date")]
         public DateTime? RequestDate { get; set; }
         [JsonPropertyName("reference")]
         public string? Reference{ get; set; }
-        [JsonPropertyName("referenceextra")]
+        [JsonPropertyName("reference_extra")]
         public string? ReferenceExtra { get; set; }
-        [JsonPropertyName("orderstatus")]
+        [JsonPropertyName("order_status")]
         public string? OrderStatus  { get; set; }
         [JsonPropertyName("note")]
         public string? Notes  { get; set; }
-        [JsonPropertyName("shiptoclient")]
+        [JsonPropertyName("ship_to_client")]
         public string? ShipToClient  { get; set; }
-        [JsonPropertyName("pickingnotes")]
+        [JsonPropertyName("picking_notes")]
         public string? PickingNotes  { get; set; }
+        [JsonPropertyName("total_amount")]
+        public float? TotalAmount  { get; set; }
+        [JsonPropertyName("total_discount")]
+        public float? TotalDiscount  { get; set; }
+        [JsonPropertyName("total_tax")]
+        public float? TotalTax  { get; set; }
+        [JsonPropertyName("total_surcharge")]
+        public float? TotalSurcharge  { get; set; }
+        [JsonPropertyName("warehouse_id")]
+        public Guid? WarehouseId  { get; set; }
+        
+        
+        //TODO:
+        //  shipment_id uuid [ref: > shipments.id]
         //TODO..
         //  ship_to_client uuid [ref: > clients.id]
         //[Required]
         //bill_to_client uuid [ref: > clients.id]
-        [JsonPropertyName("totalamount")]
-        public float? TotalAmount  { get; set; }
-        [JsonPropertyName("totaldiscount")]
-        public float? TotalDiscount  { get; set; }
-        [JsonPropertyName("totaltax")]
-        public float? TotalTax  { get; set; }
-        [JsonPropertyName("totalsurcharge")]
-        public float? TotalSurcharge  { get; set; }
-        [JsonPropertyName("warehouseid")]
-        public Warehouse? WarehouseId  { get; set; }
-        //TODO:
-        //  shipment_id uuid [ref: > shipments.id]
     }
     
     [ApiExplorerSettings(IgnoreApi = true)]
@@ -74,7 +76,7 @@ namespace DTO.Order
         [JsonPropertyName("totalsurcharge")]
         public float? TotalSurcharge  { get; set; }
         [JsonPropertyName("warehouseid")]
-        public Warehouse? WarehouseId  { get; set; }
+        public Guid? WarehouseId  { get; set; }
         //TODO:
         //  shipment_id uuid [ref: > shipments.id]
     }

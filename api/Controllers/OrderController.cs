e9bb86242536802a2f7a6623c5df0621e6a5b280
sidi <sidimo2001@gmail.com>
2024-11-16 16:02:16 +0100

    feature(order): create order in controller

---
+++ api/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+using System.Data;
+using DTO.ItemGroup;
+using DTO.Order;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    private OrderProvider _orderProvider;
+    public OrdersController(OrderProvider orderProvider)
+    {
+        _orderProvider = orderProvider;
+    }
+
+    [HttpPost()]
+    public IActionResult Create([FromBody] OrderRequest req)
+    {
+        Order? newOrder = _orderProvider.Create(req);
+        if (newOrder == null) throw new ApiFlowException("Saving new order failed.");
+
+        return Ok(new OrderResponse{         
+            Id = newOrder.Id,
+            RequestDate = newOrder.            
+            OrderDate = newOrder.OrderDate,
+            Reference = newOrder.Reference,
+            ReferenceExtra = newOrder.ReferenceExtra,
+            OrderStatus = newOrder.OrderStatus,
+            Notes = newOrder.Notes,
+            ShipToClient = newOrder.ShipToClient,
+            PickingNotes = newOrder.PickingNotes,
+            TotalAmount = newOrder.TotalAmount,
+            TotalDiscount = newOrder.TotalDiscount,
+            TotalTax = newOrder.TotalTax,
+            TotalSurcharge = newOrder.TotalSurcharge,
+            // WarehouseId = newOrder.WarehouseId,
+        });
+    }
+}

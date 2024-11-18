b5d967a21e4978d815658df68407bfac1b857a4f
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-11-18 18:43:50 +0100

    feature(shipment): Added ShipmentController with Create endpoint

---
+++ api/Controllers/ShipmentController.cs
@@ -0,0 +1,47 @@
+using DTO.Shipment;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShipmentController : ControllerBase
+{
+    private ShipmentProvider _shipmentProvider;
+
+    public ShipmentController(ShipmentProvider shipmentProvider)
+    {
+        _shipmentProvider = shipmentProvider;
+    }
+
+    [HttpPost()]
+    public IActionResult Create([FromBody] ShipmentRequest req)
+    {
+        Shipment? newShipment = _shipmentProvider.Create(req);
+        if (newShipment == null) throw new ApiFlowException("Saving new Shipment failed.");
+
+
+        return Ok(new ShipmentResponse
+        {
+            Id = newShipment.Id,
+            OrderDate = newShipment.OrderDate,
+            RequestDate = newShipment.RequestDate,
+            ShipmentDate = newShipment.ShipmentDate,
+            ShipmentType = newShipment.ShipmentType,
+            Notes = newShipment.Notes,
+            CarrierCode = newShipment.CarrierCode,
+            CarrierDescription = newShipment.CarrierDescription,
+            ServiceCode = newShipment.ServiceCode,
+            PaymentType = newShipment.PaymentType,
+            TransferMode = newShipment.TransferMode,
+            TotalPackageCount = newShipment.TotalPackageCount,
+            TotalPackageWeight = newShipment.TotalPackageWeight,
+            CreatedAt = newShipment.CreatedAt,
+            UpdatedAt = newShipment.UpdatedAt,
+            Items = newShipment.ShipmentItems?.Select(si => new ShipmentItemRR
+            {
+                ItemId = si.ItemId,
+                Amount = si.Amount
+            }).ToList()
+        });
+    }
+}

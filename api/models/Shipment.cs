6bb656f590eaa6119e502e481f60ba5d1cee9f03
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-11-18 12:43:53 +0100

    feature(shipment): updated Shipment Model with BaseModel

--- api/models/Shipment.cs
+++ api/models/Shipment.cs
@@ -1,52 +1,49 @@
+using System.ComponentModel.DataAnnotations;
 using Utils.Number;
 
-public class Shipment
+namespace Model;
+
+public class Shipment : BaseModel
 {
-    public Shipment()
-    {
-        CreatedAt = DateTime.UtcNow;
-        UpdatedAt = DateTime.UtcNow;
-    }
-
-    public Guid Id { get; set; }
-
+    public Shipment(){}
+    public Shipment(bool newInstance = false, bool isUpdate = false) : base(newInstance, isUpdate){}
     public DateTime? OrderDate { get; set; }
     public DateTime? RequestDate { get; set; }
     public DateTime? ShipmentDate { get; set; }
-
-    public required string ShipmentType { get; set; }
-
+    
+    [Required]
+    public string? ShipmentType { get; set; }
     public string? ShipmentStatus { get; set; }
-
     public string? Notes { get; set; }
-    public required string CarrierCode { get; set; }
+    
+    [Required]
+    public string? CarrierCode { get; set; }
     public string? CarrierDescription { get; set; }
-    public required string ServiceCode { get; set; }
-    public required string PaymentType { get; set; }
-    public required string TransferMode { get; set; }
+    [Required]
+    public string? ServiceCode { get; set; }
+    [Required]
+    public string? PaymentType { get; set; }
+    [Required]
+    public string? TransferMode { get; set; }
     public string? TotalPackageAmount { get; set; }
 
-    private int _totalPackageCount = 0;
-    public int? TotalPackageCount
-    {
-        get => _totalPackageCount;
-        set => _totalPackageCount = NumberUtil.EnsureNonNegative((int)value);
-    }
+    // private int _totalPackageCount = 0;
+    // public int? TotalPackageCount
+    // {
+    //     get => _totalPackageCount;
+    //     set => _totalPackageCount = NumberUtil.EnsureNonNegative((int)value);
+    // }
 
-    private decimal _totalPackageWeight = 0;
+    // private decimal _totalPackageWeight = 0;
 
-    public decimal? TotalPackageWeight
-    {
-        get => _totalPackageWeight;
-        set => _totalPackageWeight = NumberUtil.EnsureNonNegativeWithFourDecimals((decimal)value);
-    }
+    // public decimal? TotalPackageWeight
+    // {
+    //     get => _totalPackageWeight;
+    //     set => _totalPackageWeight = NumberUtil.EnsureNonNegativeWithFourDecimals((decimal)value);
+    // }
 
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-
-    /*
-    TODO: 
-        order_id: uuid required
-    */
-
+    // /*
+    // TODO: 
+    //     order_id: uuid required
+    // */
 }

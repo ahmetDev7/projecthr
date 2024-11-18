7b15b693c1162254d74840f09106dd03d0c417d3
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-11-18 18:22:43 +0100

    fix(feature): added Required tag for ShipmentId in ShipmentItem class

--- api/models/ShipmentItem.cs
+++ api/models/ShipmentItem.cs
@@ -4,11 +4,12 @@ namespace Model;
 
 public class ShipmentItem : BaseModel
 {
-    public Guid ShipmentId { get; set; }
+    [Required]
+    public Guid? ShipmentId { get; set; }
     public Shipment? Shipment { get; set; }
     
     [Required]
-    public Guid ItemId { get; set; }
+    public Guid? ItemId { get; set; }
     public Item? Item { get; set; }
 
     [Required]

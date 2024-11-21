a32b5bd471779146da6909ca9dd3a45592c99fe0
sidi <sidimo2001@gmail.com>
2024-11-21 17:32:42 +0100

    fix(order): migrations added

---
+++ api/Migrations/20241121152048_update_order_tbl.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_order_tbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalDiscount",
+                table: "Orders",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalSurcharge",
+                table: "Orders",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalTax",
+                table: "Orders",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalDiscount",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalSurcharge",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "TotalTax",
+                table: "Orders");
+        }
+    }
+}

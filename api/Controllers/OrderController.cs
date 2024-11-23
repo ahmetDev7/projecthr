a14945f4ab5a83c496cb63dc74939cd3ccc56c0b
sidi <sidimo2001@gmail.com>
2024-11-23 17:26:04 +0100

    feature(order): functionality update

--- api/Controllers/OrderController.cs
+++ api/Controllers/OrderController.cs
@@ -43,6 +43,36 @@ public class OrdersController : ControllerBase
             }).ToList()
         });
     }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Guid id,[FromBody]OrderRequest req)
+    {
+        Order? updatedOrder = _orderProvider.Update(id,req);
+        return Ok(new OrderResponse
+        {
+            Id = updatedOrder.Id,
+            OrderDate = updatedOrder.OrderDate,
+            RequestDate = updatedOrder.RequestDate,
+            Reference = updatedOrder.Reference,
+            ReferenceExtra = updatedOrder.ReferenceExtra,
+            OrderStatus = updatedOrder.OrderStatus,
+            Notes = updatedOrder.Notes,
+            PickingNotes = updatedOrder.PickingNotes,
+            TotalAmount = updatedOrder.TotalAmount,
+            TotalDiscount = updatedOrder.TotalDiscount,
+            TotalTax = updatedOrder.TotalTax,
+            TotalSurcharge = updatedOrder.TotalSurcharge,
+            WarehouseId = updatedOrder.WarehouseId,
+            CreatedAt = updatedOrder.CreatedAt,
+            UpdatedAt = updatedOrder.UpdatedAt,
+            Items = updatedOrder.OrderItems?.Select(oi => new OrderItemRequest
+            {
+                ItemId = oi.ItemId,
+                Amount = oi.Amount
+            }).ToList()
+        });
+
+    }
     [HttpGet("{id}")]
     public IActionResult ShowSingle(Guid id)
     {

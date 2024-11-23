26f441862b70a53c83257572355e325e0ecb8825
AlperenBaytimur <alperen.baytimur@gmail.com>
2024-11-23 14:56:07 +0100

    fix: small fix in the models to match the excel sheet

--- api/models/Address.cs
+++ api/models/Address.cs
@@ -5,9 +5,7 @@ public class Address : BaseModel
     public Address() { }
     public Address(bool newInstance = false, bool isUpdate = false) : base(newInstance, isUpdate) { }
     
-    [Required]
     public string? Street { get; set; }
-    [Required]
     public string? HouseNumber { get; set; }
     public string? HouseNumberExtension { get; set; }
     public string? HouseNumberExtensionExtra { get; set; }

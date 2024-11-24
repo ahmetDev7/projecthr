bfcaff1aab65078b8caab5d3a8e547dde51cfd61
AlperenBaytimur <alperen.baytimur@gmail.com>
2024-11-24 15:19:01 +0100

    chore: supplier branch refinement

--- api/providers/SupplierProvider.cs
+++ api/providers/SupplierProvider.cs
@@ -15,37 +15,6 @@ public class SupplierProvider : BaseProvider<Supplier>
         _contactProvider = contactProvider;
     }
 
-    public override Supplier? Create(BaseDTO createValues)
-    {
-        SupplierRequest? req = createValues as SupplierRequest;
-        var allSuppliers = _db.Suppliers.Include(c => c.Contact).Include(a => a.Address).ToList();
-
-        if(allSuppliers.Any(s => s.Reference == req.Reference)) throw new ApiFlowException("Could not process create supplier request. Supplier with this reference code already exists.");
-        if (req == null) throw new ApiFlowException("Could not process create supplier request. Save new supplier failed.");
-
-        Contact? relatedContact = _contactProvider.GetOrCreateContact(req);
-        Address? relatedAddress = _addressProvider.GetOrCreateAddress(req);
-
-        Supplier newSupplier = new Supplier(newInstance: true)
-        {
-            Code = req.Code,
-            Name = req.Name,
-            Reference = req.Reference,
-            ContactId = relatedContact.Id,
-            AddressId = relatedAddress.Id,
-        };
-        
-
-        ValidateModel(newSupplier);
-        _db.Suppliers.Add(newSupplier);
-        SaveToDBOrFail();
-
-        newSupplier.Contact =relatedContact;
-        newSupplier.Address =relatedAddress;
-
-        return newSupplier;
-    }
-
     public List<Item> GetItemsByReferenceCode(string Reference)
     {
         var itemsSpecificSupplier = _db.Items.Where(l => l.SupplierReferenceCode == Reference).ToList();

36bec8f906c3cabafceb1a482726aa3a5a089579
Sharif-C <sharifali.786@hotmail.com>
2024-11-26 14:17:04 +0100

    fix: re-add item_line and item_type tables relating to items

---
+++ api/Migrations/20241126131609_create_item_line_item_type_tables_and_releate_to_items_tbl.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_item_line_item_type_tables_and_releate_to_items_tbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ItemLineId",
+                table: "Items",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ItemTypeId",
+                table: "Items",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ItemLines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemLines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemLineId",
+                table: "Items",
+                column: "ItemLineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemTypeId",
+                table: "Items",
+                column: "ItemTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_ItemLines_ItemLineId",
+                table: "Items",
+                column: "ItemLineId",
+                principalTable: "ItemLines",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_ItemTypes_ItemTypeId",
+                table: "Items",
+                column: "ItemTypeId",
+                principalTable: "ItemTypes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_ItemLines_ItemLineId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_ItemTypes_ItemTypeId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "ItemLines");
+
+            migrationBuilder.DropTable(
+                name: "ItemTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_ItemLineId",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_ItemTypeId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ItemLineId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ItemTypeId",
+                table: "Items");
+        }
+    }
+}

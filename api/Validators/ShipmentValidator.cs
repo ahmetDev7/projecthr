514c7af01af7be57af887ac596e23e504fad6576
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-11-26 13:21:15 +0100

    feature(shipment): Added validation for order_id if not exists

--- api/Validators/ShipmentValidator.cs
+++ api/Validators/ShipmentValidator.cs
@@ -6,7 +6,13 @@ public class ShipmentValidator : AbstractValidator<Shipment>
     {
         RuleFor(shipment => shipment.OrderId)
             .NotNull().WithMessage("order_id is required.")
-            .NotEmpty().WithMessage("order_id cannot be empty.");
+            .NotEmpty().WithMessage("order_id cannot be empty.")
+            .Custom((orderId, context) => {
+                if (orderId != null && !db.Orders.Any(o => o.Id == orderId))
+                {
+                    context.AddFailure("order_id", "The provided order_id does not exist.");
+                }
+            });
         RuleFor(shipment => shipment.ShipmentType)
             .NotNull().WithMessage("shipment_type is required.")
             .NotEmpty().WithMessage("shipment_type cannot be empty.");

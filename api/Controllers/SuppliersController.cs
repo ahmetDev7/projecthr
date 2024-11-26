82adfe025dd6c5a9876344d3b833baa92b4f9085
AlperenBaytimur <alperen.baytimur@gmail.com>
2024-11-26 17:32:54 +0100

    fix: function order conventions

--- api/Controllers/SuppliersController.cs
+++ api/Controllers/SuppliersController.cs
@@ -12,68 +12,42 @@ public class SuppliersController : ControllerBase
         _supplierProvider = supplierProvider;
     }
 
-    [HttpGet]
-    public IActionResult ShowAll() => Ok(_supplierProvider.GetAll().Select(ig => new SupplierResponse
+    [HttpPost]
+    public IActionResult Create(SupplierRequest request)
     {
-        Id = ig.Id,
-        Code = ig.Code,
-        Name = ig.Name,
-        Reference = ig.Reference,
-        Contact = new ContactDTO
+        Supplier? supplier = _supplierProvider.Create(request);
+        if (supplier == null) throw new ApiFlowException("Saving new Supplier failed.");
+
+
+        return Ok(new
         {
-            Name = ig.Contact.Name,
-            Email = ig.Contact.Email,
-            Phone = ig.Contact.Phone
-        },
-        Address = new AddressDTO
-        {
-            Street = ig.Address.Street,
-            HouseNumber = ig.Address.HouseNumber,
-            HouseNumberExtension = ig.Address.HouseNumberExtension,
-            HouseNumberExtensionExtra = ig.Address.HouseNumberExtensionExtra,
-            ZipCode = ig.Address.ZipCode,
-            City = ig.Address.City,
-            Province = ig.Address.Province,
-            CountryCode = ig.Address.CountryCode,
-        },
-        CreatedAt = ig.CreatedAt,
-        UpdatedAt = ig.UpdatedAt
-        
-    }).ToList());
-
-
-   [HttpGet("{id}")]
-    public IActionResult GetById(Guid id)
-    {
-        Supplier? supplier = _supplierProvider.GetById(id);
-
-        if (supplier == null) return NotFound(new { message = $"Supplier not found for id '{id}'" });
-
-        return Ok(new SupplierResponse
-        {
-            Id = supplier.Id,
-            Name = supplier.Name,
-            Code = supplier.Code,
-            Reference = supplier.Reference,
-            Contact = new ContactDTO
+            message = "Supplier created successfully.",
+            created_supplier = new SupplierResponse
             {
-                Name = supplier.Contact.Name,
-                Email = supplier.Contact.Email,
-                Phone = supplier.Contact.Phone
-            },
-            Address = new AddressDTO
-            {
-                Street = supplier.Address.Street,
-                HouseNumber = supplier.Address.HouseNumber,
-                HouseNumberExtension = supplier.Address.HouseNumberExtension,
-                HouseNumberExtensionExtra = supplier.Address.HouseNumberExtensionExtra,
-                ZipCode = supplier.Address.ZipCode,
-                City = supplier.Address.City,
-                Province = supplier.Address.Province,
-                CountryCode = supplier.Address.CountryCode,
-            },
-            CreatedAt = supplier.CreatedAt,
-            UpdatedAt = supplier.UpdatedAt
+                Id = supplier.Id,
+                Code = supplier.Code,
+                Name = supplier.Name,
+                Reference = supplier.Reference,
+                Contact = new ContactDTO
+                {
+                    Name = supplier.Contact.Name,
+                    Email = supplier.Contact.Email,
+                    Phone = supplier.Contact.Phone
+                },
+                Address = new AddressDTO
+                {
+                    Street = supplier.Address.Street,
+                    HouseNumber = supplier.Address.HouseNumber,
+                    HouseNumberExtension = supplier.Address.HouseNumberExtension,
+                    HouseNumberExtensionExtra = supplier.Address.HouseNumberExtensionExtra,
+                    ZipCode = supplier.Address.ZipCode,
+                    City = supplier.Address.City,
+                    Province = supplier.Address.Province,
+                    CountryCode = supplier.Address.CountryCode,
+                },
+                CreatedAt = supplier.CreatedAt,
+                UpdatedAt = supplier.UpdatedAt
+            }
         });
     }
 
@@ -114,4 +88,67 @@ public class SuppliersController : ControllerBase
             }
         });
     }
+
+   [HttpGet("{id}")]
+    public IActionResult ShowSingle(Guid id)
+    {
+        Supplier? supplier = _supplierProvider.GetById(id);
+
+        if (supplier == null) return NotFound(new { message = $"Supplier not found for id '{id}'" });
+
+        return Ok(new SupplierResponse
+        {
+            Id = supplier.Id,
+            Name = supplier.Name,
+            Code = supplier.Code,
+            Reference = supplier.Reference,
+            Contact = new ContactDTO
+            {
+                Name = supplier.Contact.Name,
+                Email = supplier.Contact.Email,
+                Phone = supplier.Contact.Phone
+            },
+            Address = new AddressDTO
+            {
+                Street = supplier.Address.Street,
+                HouseNumber = supplier.Address.HouseNumber,
+                HouseNumberExtension = supplier.Address.HouseNumberExtension,
+                HouseNumberExtensionExtra = supplier.Address.HouseNumberExtensionExtra,
+                ZipCode = supplier.Address.ZipCode,
+                City = supplier.Address.City,
+                Province = supplier.Address.Province,
+                CountryCode = supplier.Address.CountryCode,
+            },
+            CreatedAt = supplier.CreatedAt,
+            UpdatedAt = supplier.UpdatedAt
+        });
+    }
+    [HttpGet]
+    public IActionResult ShowAll() => Ok(_supplierProvider.GetAll().Select(ig => new SupplierResponse
+    {
+        Id = ig.Id,
+        Code = ig.Code,
+        Name = ig.Name,
+        Reference = ig.Reference,
+        Contact = new ContactDTO
+        {
+            Name = ig.Contact.Name,
+            Email = ig.Contact.Email,
+            Phone = ig.Contact.Phone
+        },
+        Address = new AddressDTO
+        {
+            Street = ig.Address.Street,
+            HouseNumber = ig.Address.HouseNumber,
+            HouseNumberExtension = ig.Address.HouseNumberExtension,
+            HouseNumberExtensionExtra = ig.Address.HouseNumberExtensionExtra,
+            ZipCode = ig.Address.ZipCode,
+            City = ig.Address.City,
+            Province = ig.Address.Province,
+            CountryCode = ig.Address.CountryCode,
+        },
+        CreatedAt = ig.CreatedAt,
+        UpdatedAt = ig.UpdatedAt
+        
+    }).ToList());
 }

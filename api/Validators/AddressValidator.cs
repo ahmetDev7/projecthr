90a34ba535f748c82be35b2f866ba0c353e0bed4
sidi <sidimo2001@gmail.com>
2024-11-30 13:48:02 +0100

    fix(address): to snake_case

--- api/Validators/AddressValidator.cs
+++ api/Validators/AddressValidator.cs
@@ -5,19 +5,19 @@ public class AddressValidator : AbstractValidator<Address>
     public AddressValidator()
     {
         RuleFor(address => address.Street)
-            .NotNull().WithMessage("Street is required.")
-            .NotEmpty().WithMessage("Street cannot be empty.");
+            .NotNull().WithMessage("street is required.")
+            .NotEmpty().WithMessage("street cannot be empty.");
         RuleFor(address => address.HouseNumber)
-            .NotNull().WithMessage("House number is required.")
-            .NotEmpty().WithMessage("House number cannot be empty.");
+            .NotNull().WithMessage("house_number is required.")
+            .NotEmpty().WithMessage("house_number cannot be empty.");
         RuleFor(address => address.ZipCode)
-            .NotNull().WithMessage("ZipCode is required.")
-            .NotEmpty().WithMessage("ZipCode cannot be empty.");
+            .NotNull().WithMessage("zipcode is required.")
+            .NotEmpty().WithMessage("zipcode cannot be empty.");
         RuleFor(address => address.City)
-            .NotNull().WithMessage("City is required.")
-            .NotEmpty().WithMessage("City cannot be empty.");
+            .NotNull().WithMessage("city is required.")
+            .NotEmpty().WithMessage("city cannot be empty.");
         RuleFor(address => address.CountryCode)
-            .NotNull().WithMessage("Country code is required.")
-            .NotEmpty().WithMessage("Country code cannot be empty.");
+            .NotNull().WithMessage("country_code is required.")
+            .NotEmpty().WithMessage("country_code cannot be empty.");
     }
 }

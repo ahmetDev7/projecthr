c28ec3a69f8280e524d1218b8304f5b0a087a3f7
Sharif-C <sharifali.786@hotmail.com>
2024-12-02 00:43:34 +0100

    feature(Transfers): implement validator and provider in Program.cs
    INFSCP01-8

--- api/Program.cs
+++ api/Program.cs
@@ -100,6 +100,7 @@ builder.Services.AddTransient<ItemGroupProvider>();
 builder.Services.AddTransient<OrderProvider>();
 builder.Services.AddTransient<ShipmentProvider>();
 builder.Services.AddTransient<SupplierProvider>();
+builder.Services.AddTransient<TransferProvider>();
 
 builder.Services.AddScoped<IValidator<Supplier>, SupplierValidator>();
 builder.Services.AddScoped<IValidator<Location>, LocationValidator>();
@@ -108,9 +109,8 @@ builder.Services.AddScoped<IValidator<ItemGroup>, ItemGroupValidator>();
 builder.Services.AddScoped<IValidator<Order>, OrderValidator>();
 builder.Services.AddScoped<IValidator<Shipment>, ShipmentValidator>();
 builder.Services.AddScoped<IValidator<Contact>, ContactValidator>();
-
-
 builder.Services.AddScoped<IValidator<Address>, AddressValidator>();
+builder.Services.AddScoped<IValidator<Transfer>, TransferValidator>();
 
 builder.Services.AddControllers();
 

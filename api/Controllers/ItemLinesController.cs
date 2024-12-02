d80673325de2369230a053448522132496eb7411
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-12-02 12:57:45 +0100

    feature(item_line): Added ShowSingle functionality to ItemLinesController

--- api/Controllers/ItemLinesController.cs
+++ api/Controllers/ItemLinesController.cs
@@ -31,4 +31,21 @@ public class ItemLinesController : ControllerBase
             }
         });
     }
+
+    [HttpGet("{id}")]
+    public IActionResult ShowSingle(Guid id)
+    {
+        ItemLine? foundItemLine = _itemLinesProvider.GetById(id);
+
+        return (foundItemLine == null)
+            ? NotFound(new { message = $"Item Line not found for id '{id}'" })
+            : Ok(new ItemLineResponse
+            {
+                Id = foundItemLine.Id,
+                Name = foundItemLine.Name,
+                Description = foundItemLine.Description,
+                CreatedAt = foundItemLine.CreatedAt,
+                UpdatedAt = foundItemLine.UpdatedAt
+            });
+    }
 }

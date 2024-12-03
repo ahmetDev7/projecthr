02f79b16c393fe5b46530922a5ae412e14b6f25a
Sharif-C <sharifali.786@hotmail.com>
2024-12-03 17:37:03 +0100

    chore(Transfers): add strict validation rules
    INFSCP01-8

--- api/Validators/TransferValidator.cs
+++ api/Validators/TransferValidator.cs
@@ -4,9 +4,58 @@ public class TransferValidator : AbstractValidator<Transfer>
 {
     public TransferValidator(AppDbContext db)
     {
+        RuleFor(transfer => transfer).Custom((transfer, context) =>
+        {
+            // check if transfer_from and transfer_to are not the same
+            if(transfer.TransferFromId == transfer.TransferToId){
+                context.AddFailure("transfer_from_id and transfer_to_id", "transfer_to_id and transfer_from_id cannot be the same.");
+                return;
+            }
+
+            // check if transfer_from the locations exists
+            bool fromLocationExists = db.Locations.Any(l => l.Id == transfer.TransferFromId);
+            if (fromLocationExists == false)
+            {
+                context.AddFailure("items", "The specified transfer source location (transfer_from_id) does not exist, and as a result, the location for the provided items cannot be found.");
+            }
+
+            if(transfer.TransferItems == null || transfer.TransferItems.Count == 0) return;
+
+            foreach (TransferItem transferItem in transfer.TransferItems)
+            {
+                Guid? itemId = transferItem.ItemId;
+
+                // Checks if the item_id related to the inventory_id exists
+                Inventory? foundInventory = db.Inventories.FirstOrDefault(i => i.ItemId == itemId);
+                if (foundInventory == null) {
+                    context.AddFailure("items", $"The specified item (ID: '{itemId}') has no inventory, and therefore, no allocated location exists for this item.");
+                    return;
+                }
+                
+                 // check if the inventory_id is on the location (from_transfer_id)
+                Location? locationOfTransferFrom = db.Locations.FirstOrDefault(l => l.InventoryId == foundInventory.Id && l.Id == transfer.TransferFromId);
+                if (locationOfTransferFrom == null) {
+                    context.AddFailure("items", $"The specified item (ID: '{itemId}') is not available at the transfer source location (Location ID: '{transfer.TransferFromId}').");
+                    return;
+                }
+
+                // check if the inventory_id is null on the location (transfer_to_id)
+                bool locationToIsEmpty = db.Locations.Any(l => l.Id == transfer.TransferToId && l.InventoryId == null);
+                if (locationToIsEmpty == false){
+                    context.AddFailure("items", "The destination location (transfer_to_id) already contains an inventory item and cannot be used for this transfer.");
+                }      
+
+                // check if the selected on_hand amount is lower or equal to the selected amount
+                int? toTransferAmount = transferItem.Amount;  
+                if(toTransferAmount > locationOfTransferFrom.OnHand){
+                    context.AddFailure("items", "The transfer amount exceeds the available inventory at the source location. Please adjust the amount to match the on-hand quantity.");
+                }
+
+            }
+        });
+
         RuleFor(transfer => transfer.TransferStatus)
-            .NotNull().WithMessage("transfer_status is required.")
-            .NotEmpty().WithMessage($"Invalid transfer_status. Allowed values are ({EnumUtil.EnumsToString<TransferStatus>()})");
+            .NotNull().WithMessage($"Invalid transfer_status. Allowed values are ({EnumUtil.EnumsToString<TransferStatus>()})");
 
         RuleFor(transfer => transfer.TransferFromId)
             .NotNull().WithMessage("transfer_from_id is required.")
@@ -34,8 +83,7 @@ public class TransferValidator : AbstractValidator<Transfer>
         .ChildRules(item =>
         {
             item.RuleFor(item => item.Amount)
-                .GreaterThanOrEqualTo(1)
-                .WithMessage("amount must be at least 1.");
+                .GreaterThanOrEqualTo(1).WithMessage("amount must be at least 1.");
 
             item.RuleFor(item => item.ItemId)
                 .NotNull().WithMessage("item_id is required.")
@@ -51,10 +99,23 @@ public class TransferValidator : AbstractValidator<Transfer>
 
 
         RuleFor(transfer => transfer.TransferItems)
-            .Custom((transferItems, context) => {
-                if(transferItems != null && CollectionUtil.ContainsDuplicateId(transferItems.Select(i => i.ItemId).ToList())) {
+            .NotNull().WithMessage("items is required.")
+            .NotEmpty().WithMessage("items cannot be empty.")
+            .Custom((transferItems, context) =>
+            {
+                if (transferItems != null && CollectionUtil.ContainsDuplicateId(transferItems.Select(i => i.ItemId).ToList()))
+                {
                     context.AddFailure("transfer_items must have unique item IDs. Duplicate item IDs are not allowed.");
                 }
             });
+
+        RuleForEach(transfer => transfer.TransferItems).ChildRules(item =>
+        {
+            item.RuleFor(item => item.ItemId)
+                .Custom((itemId, context) =>
+                {
+
+                });
+        });
     }
 }

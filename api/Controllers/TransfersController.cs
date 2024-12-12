020e87edd7e6a5c34eee35882786c5678436382d
Sharif-C <sharifali.786@hotmail.com>
2024-12-12 22:38:10 +0100

    feature(Transfers)!: implement create, getById and commit with controllers, providers, rest file and validator

--- api/Controllers/TransfersController.cs
+++ api/Controllers/TransfersController.cs
@@ -12,7 +12,7 @@ public class TransfersController : ControllerBase
     }
 
     [HttpPost()]
-    public IActionResult Create([FromBody] TransferRequest req)
+    public IActionResult Create([FromBody] TransferRequestCreate req)
     {
         Transfer? newTransfer = _transferProvider.Create(req);
         if (newTransfer == null) throw new ApiFlowException("Saving new transfer failed.");
@@ -27,7 +27,8 @@ public class TransfersController : ControllerBase
                 TransferFromId = newTransfer.TransferFromId,
                 TransferToId = newTransfer.TransferToId,
                 TransferStatus = newTransfer.TransferStatus.ToString(),
-                Items = newTransfer.TransferItems?.Select(ti => new TransferItemDTO(){
+                Items = newTransfer.TransferItems?.Select(ti => new TransferItemDTO()
+                {
                     ItemId = ti.ItemId,
                     Amount = ti.Amount,
                 }).ToList(),
@@ -37,5 +38,54 @@ public class TransfersController : ControllerBase
         });
     }
 
+    [HttpPut("{transferId}")]
+    public IActionResult CommitTransfer(Guid transferId)
+    {
+        Transfer? commitedTransfer = _transferProvider.CommitTransfer(transferId);
+        if (commitedTransfer == null) throw new ApiFlowException("Commting transfer failed.");
 
+        return Ok(new
+        {
+            message = "Transfer commited!",
+            created_transfer = new TransferResponse
+            {
+                Id = commitedTransfer.Id,
+                Reference = commitedTransfer.Reference,
+                TransferFromId = commitedTransfer.TransferFromId,
+                TransferToId = commitedTransfer.TransferToId,
+                TransferStatus = commitedTransfer.TransferStatus.ToString(),
+                Items = commitedTransfer.TransferItems?.Select(ti => new TransferItemDTO()
+                {
+                    ItemId = ti.ItemId,
+                    Amount = ti.Amount,
+                }).ToList(),
+                CreatedAt = commitedTransfer.CreatedAt,
+                UpdatedAt = commitedTransfer.UpdatedAt,
+            }
+        });
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ShowSingle(Guid id)
+    {
+        Transfer? foundTransfer = _transferProvider.GetById(id);
+
+        return (foundTransfer == null)
+            ? NotFound(new { message = $"Transfer not found for id '{id}'" })
+            : Ok(new TransferResponse
+            {
+                Id = foundTransfer.Id,
+                Reference = foundTransfer.Reference,
+                TransferFromId = foundTransfer.TransferFromId,
+                TransferToId = foundTransfer.TransferToId,
+                TransferStatus = foundTransfer.TransferStatus.ToString(),
+                Items = foundTransfer.TransferItems?.Select(ti => new TransferItemDTO()
+                {
+                    ItemId = ti.ItemId,
+                    Amount = ti.Amount,
+                }).ToList(),
+                CreatedAt = foundTransfer.CreatedAt,
+                UpdatedAt = foundTransfer.UpdatedAt,
+            });
+    }
 }
--- api/Validators/TransferValidator.cs
+++ api/Validators/TransferValidator.cs
@@ -2,63 +2,145 @@ using FluentValidation;
 
 public class TransferValidator : AbstractValidator<Transfer>
 {
-    public TransferValidator(AppDbContext db)
+    public TransferValidator(AppDbContext db, InventoriesProvider inventoriesProvider, LocationsProvider locationsProvider)
     {
         RuleFor(transfer => transfer).Custom((transfer, context) =>
         {
             // check if transfer_from and transfer_to are not the same
-            if(transfer.TransferFromId == transfer.TransferToId){
+            if (transfer.TransferFromId == transfer.TransferToId)
+            {
                 context.AddFailure("transfer_from_id and transfer_to_id", "transfer_to_id and transfer_from_id cannot be the same.");
                 return;
             }
 
+            if (transfer.TransferItems == null || transfer.TransferItems.Count == 0) return;
+
             bool fromDock = transfer.TransferFromId == null;
             bool toDock = transfer.TransferToId == null;
-            
-            if(fromDock == false){
-                // check if transfer_from the locations exists
-                bool fromLocationExists = db.Locations.Any(l => l.Id == transfer.TransferFromId);
-                if (fromLocationExists == false)
+
+            bool fromLocation = !fromDock;
+            bool toLocation = !toDock;
+
+            Warehouse? warehouseFrom = null;
+            Warehouse? warehouseTo = null;
+
+            if (fromLocation && transfer.TransferFromId.HasValue)
+            {
+                Location? locationFrom = locationsProvider.GetById(id: transfer.TransferFromId.Value, includeWarehouse: true);
+                if (locationFrom == null) return;
+                warehouseFrom = locationFrom.Warehouse;
+                if (warehouseFrom == null)
                 {
-                    context.AddFailure("items", "The specified transfer source location (transfer_from_id) does not exist, and as a result, the location for the provided items cannot be found.");
+                    context.AddFailure("transfer_from_id", $"Warehouse not found");
                     return;
                 }
             }
 
-            if(transfer.TransferItems == null || transfer.TransferItems.Count == 0) return;
-            
+            if (toLocation && transfer.TransferToId.HasValue)
+            {
+                Location? locationTo = locationsProvider.GetById(id: transfer.TransferToId.Value, includeWarehouse: true);
+                if (locationTo == null) return;
+                warehouseTo = locationTo.Warehouse;
+
+                if (warehouseTo == null)
+                {
+                    context.AddFailure("transfer_to_id", $"Warehouse not found");
+                    return;
+                }
+            }
+
+            if (fromLocation && toLocation && warehouseFrom.Id != warehouseTo.Id)
+            {
+                context.AddFailure("transfer_from_id and transfer_to_id", $"Both locations must be in the same warehouse");
+                return;
+            }
+
             foreach (TransferItem transferItem in transfer.TransferItems)
             {
-                Guid? itemId = transferItem.ItemId;
+                Guid? itemId = transferItem.ItemId.Value;
+                Inventory? foundInventory = null;
 
                 // Checks if the item_id related to the inventory_id exists
-                Inventory? foundInventory = db.Inventories.FirstOrDefault(i => i.ItemId == itemId);
-                if (foundInventory == null) {
-                    context.AddFailure("items", $"The specified item (ID: '{itemId}') has no inventory, and therefore, no allocated location exists for this item.");
+                foundInventory = db.Inventories.FirstOrDefault(i => i.ItemId == itemId);
+                if (foundInventory == null)
+                {
+                    context.AddFailure("items", $"The specified item (ID: {itemId}) has no inventory, and therefore, no allocated location exists for this item.");
                     return;
                 }
 
-                if(fromDock == false){
+                if (fromLocation)
+                {
                     // check if the inventory_id is on the location (from_transfer_id)
                     Location? locationOfTransferFrom = db.Locations.FirstOrDefault(l => l.InventoryId == foundInventory.Id && l.Id == transfer.TransferFromId);
-                    if (locationOfTransferFrom == null) {
-                        context.AddFailure("items", $"The specified item (ID: '{itemId}') is not available at the transfer source location (Location ID: '{transfer.TransferFromId}').");
+                    if (locationOfTransferFrom == null)
+                    {
+                        context.AddFailure("items", $"item is not from this location {transfer.TransferFromId}");
                         return;
                     }
 
                     // check if the selected on_hand amount is lower or equal to the selected amount
-                    int? toTransferAmount = transferItem.Amount;  
-                    if(toTransferAmount > locationOfTransferFrom.OnHand){
+                    int? transferAmount = transferItem.Amount;
+                    if (transferAmount > locationOfTransferFrom.OnHand)
+                    {
                         context.AddFailure("items", "The transfer amount exceeds the available inventory at the source location. Please adjust the amount to match the on-hand quantity.");
+                        return;
                     }
                 }
 
-                if(toDock == false){
-                    // check if the inventory_id is null on the location (transfer_to_id)
-                    bool locationToIsEmpty = db.Locations.Any(l => l.Id == transfer.TransferToId && l.InventoryId == null);
-                    if (locationToIsEmpty == false){
-                        context.AddFailure("items", "The destination location (transfer_to_id) already contains an inventory item and cannot be used for this transfer.");
-                    }  
+                if (toLocation)
+                {
+                    // get inventory_id from location
+                    Location? transferToLocation = db.Locations.FirstOrDefault(l => l.Id == transfer.TransferToId);
+                    if (transferToLocation == null) return;
+
+                    // if inventory_id is different from the item.InventoryId then show exception.
+                    if (transferToLocation.InventoryId.HasValue && foundInventory?.Id != transferToLocation.InventoryId)
+                    {
+                        context.AddFailure("items", $"There already is another item on this location {transfer.TransferToId}");
+                        return;
+                    }
+                }
+
+                int? totalAmount = null;
+                if (fromDock || toDock)
+                {
+                    totalAmount = transfer.TransferItems.Sum(i => i.Amount);
+                }
+
+                if (fromDock)
+                {
+                    // Find the item in the destination dock
+                    DockItem fromDockItem = db.DockItems.FirstOrDefault(di => di.DockId == warehouseTo.Dock.Id && di.ItemId == itemId);
+
+                    // Validate item existence in the dock
+                    if (fromDockItem == null)
+                    {
+                        context.AddFailure("items", $"Item not in dock");
+                        return;
+                    }
+
+                    // Ensure the requested amount does not exceed available quantity
+                    if (totalAmount > fromDockItem.Amount)
+                    {
+                        context.AddFailure(
+                            "items",
+                            $"The selected amount exceeds the available quantity on the dock. Current dock quantity: {fromDockItem.Amount}. Attempted quantity: {totalAmount}."
+                        );
+                        return;
+                    }
+                }
+
+                if (toDock)
+                {
+                    // check if dock capacity is lower than 50 based on the total item amount in transfer and the fromDock location
+                    int? totalOnDock = db.DockItems.Where(di => di.DockId == warehouseFrom.Dock.Id).Sum(di => di.Amount) ?? 0;
+                    int? spaceAvailable = Dock.CAPCITY - totalOnDock;
+
+                    if (totalAmount > spaceAvailable)
+                    {
+                        context.AddFailure("items", $"The selected amount exceeds the dock's capacity. The maximum capacity is 50, with only {spaceAvailable} space(s) remaining. You attempted to add {totalAmount} item(s). Please adjust the quantity to fit within the available space.");
+                        return;
+                    }
                 }
             }
         });
@@ -67,22 +149,18 @@ public class TransferValidator : AbstractValidator<Transfer>
             .NotNull().WithMessage($"Invalid transfer_status. Allowed values are ({EnumUtil.EnumsToString<TransferStatus>()})");
 
         RuleFor(transfer => transfer.TransferFromId)
-            .NotNull().WithMessage("transfer_from_id is required.")
-            .NotEmpty().WithMessage("transfer_from_id cannot be empty.")
             .Custom((TransferFromId, context) =>
             {
-                if (TransferFromId != null && !db.Locations.Any(l => l.Id == TransferFromId))
+                if (TransferFromId.HasValue && !db.Locations.Any(l => l.Id == TransferFromId))
                 {
                     context.AddFailure("transfer_from_id", "The provided transfer_from_id does not exist.");
                 }
             });
 
         RuleFor(transfer => transfer.TransferToId)
-            .NotNull().WithMessage("transfer_to_id is required.")
-            .NotEmpty().WithMessage("transfer_to_id cannot be empty.")
             .Custom((TransferToId, context) =>
             {
-                if (TransferToId != null && !db.Locations.Any(l => l.Id == TransferToId))
+                if (TransferToId.HasValue && !db.Locations.Any(l => l.Id == TransferToId))
                 {
                     context.AddFailure("transfer_to_id", "The provided transfer_to_id does not exist.");
                 }
@@ -117,14 +195,5 @@ public class TransferValidator : AbstractValidator<Transfer>
                     context.AddFailure("transfer_items must have unique item IDs. Duplicate item IDs are not allowed.");
                 }
             });
-
-        RuleForEach(transfer => transfer.TransferItems).ChildRules(item =>
-        {
-            item.RuleFor(item => item.ItemId)
-                .Custom((itemId, context) =>
-                {
-
-                });
-        });
     }
 }

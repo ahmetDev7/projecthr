5bcdc1ed68bffbb42b768d0f8f7661492eca1e9f
Sharif-C <sharifali.786@hotmail.com>
2024-12-25 00:56:07 +0100

    fix(inventory): add migrations with calculated fields

--- api/Migrations/20241224235506_update_tbl_inventory_calculated_fields.Designer.cs
+++ api/Migrations/20241224235506_update_tbl_inventory_calculated_fields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241224235506_update_tbl_inventory_calculated_fields")]
+    partial class update_tbl_inventory_calculated_fields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

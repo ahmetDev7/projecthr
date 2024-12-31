898712d9645e645ce897366b41b3211d0881a987
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2024-12-31 23:30:04 +0100

    feature(integration tests): added ItemTypes Integration Test

---
+++ api.IntegrationTests/ItemTypesIntegrationTests.cs
@@ -0,0 +1,36 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using Newtonsoft.Json.Linq;
+
+namespace api.IntegrationTests
+{
+    public class ItemTypesIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ItemTypesIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAll_ItemTypes_ReturnsOkAndNonEmptyList()
+        {
+            // Arrange
+            var url = "/itemtypes";
+
+            // Act
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var jsonArray = JArray.Parse(responseBody);
+
+            Assert.True(jsonArray.Count > 0, "Expected non-empty list of item types.");
+        }
+    }
+}

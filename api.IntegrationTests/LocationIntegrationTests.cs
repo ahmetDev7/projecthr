250e86226444f35d44f9562c530ab984cfabccf0
AlperenBaytimur <alperen.baytimur@gmail.com>
2025-01-10 21:17:38 +0100

    feature(IntegrationtestsLocation) Ik heb integratie testen gemaakt voor Location en ook de bijbehorende seeds erbij gemaakt

---
+++ api.IntegrationTests/LocationIntegrationTests.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace api.IntegrationTests
+{
+    public class LocationIntegrationTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl = "/api/Locations";
+
+        public LocationIntegrationTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task GetLocation_LocationExist_ReturnsSuccesWithLocations()
+        {
+            var response = await _httpClient.GetAsync(_baseUrl);
+            var result = await response.Content.ReadFromJsonAsync<List<Location>>();
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task GetSingleLocation_ReturnsSuccesWithLocation()
+        {
+            var response = await _httpClient.GetAsync(_baseUrl + "/91629396-1d08-4f77-9049-c49216870112");
+            var result = await response.Content.ReadFromJsonAsync<Location>();
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task DeleteLocation_ReturnSuccesOKResponse()
+        {
+            var response = await _httpClient.DeleteAsync(_baseUrl + "/68b1efa8-8cbe-4dae-867f-40384954c5cc");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetSingleLocation_ReturnsNotFound404_wrong_ID()
+        {
+            var response = await _httpClient.GetAsync(_baseUrl + "/59a2a43e-2e9f-42a5-9817-849c70a78e20");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+        
+        [Fact]
+        public async Task DeleteLocation_ReturnsNotFound()
+        {
+            var response = await _httpClient.DeleteAsync(_baseUrl + "/59a2a43e-2e9f-42a5-9817-849c70a78e20");
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
--- api.IntegrationTests/Seeding.cs
+++ api.IntegrationTests/Seeding.cs
@@ -10,6 +10,9 @@ namespace api.IntegrationTests
         public static void IntializeTestDB(AppDbContext db)
         {
             db.ItemTypes.AddRange(GetItemTypes());
+            db.Locations.AddRange(GetLocations());
+            db.Addresses.AddRange(GetAddresses());
+            db.Warehouses.AddRange(GetWarehouses());
             db.SaveChanges();
         }
 
@@ -21,5 +24,81 @@ namespace api.IntegrationTests
                 new ItemType(newInstance: true) { Id = Guid.Parse("23a60ea1-4471-4f1f-b0f5-a25527121647"), Name = "Item Type 3", Description = "Description Item Type 3" }
             };
         }
+        private static List<Location> GetLocations()
+        {   
+            return new List<Location>(){
+                new Location(newInstance: true)
+                {
+                    Id = Guid.Parse("91629396-1d08-4f77-9049-c49216870112"),
+                    Row = "2",
+                    Rack = "3",
+                    Shelf = "5",
+                    WarehouseId = Guid.Parse("78b5c277-8784-4eb6-ac7d-a1f07dab6e49")
+                },
+                new Location(newInstance: true)
+                { 
+                    Id = Guid.Parse("e6786fad-435b-460f-b6dd-11dd32b3b6a6"),
+                    Row = "2",
+                    Rack = "3",
+                    Shelf = "5",
+                    WarehouseId = Guid.Parse("78b5c277-8784-4eb6-ac7d-a1f07dab6e49")
+                },
+                new Location(newInstance: true)
+                {
+                    Id = Guid.Parse("68b1efa8-8cbe-4dae-867f-40384954c5cc"),
+                    Row = "2",
+                    Rack = "3",
+                    Shelf = "5",
+                    WarehouseId = Guid.Parse("78b5c277-8784-4eb6-ac7d-a1f07dab6e49")
+                }
+            };
+        }
+        private static List<Address> GetAddresses()
+        {
+            return new List<Address>()
+            {
+                new Address(newInstance: true)
+                {
+                    Id = Guid.Parse("cefc9e60-7d37-41f5-b3c8-3144894f207e"),
+                    Street = "Street 1",
+                    HouseNumber = "1",
+                    HouseNumberExtension = "A",
+                    HouseNumberExtensionExtra = "B",
+                    ZipCode = "1234AB",
+                    City = "Amsterdam",
+                    Province = "Noord-Holland",
+                    CountryCode = "NL"
+                },
+                new Address(newInstance: true)
+                {
+                    Id = Guid.Parse("2ff2c789-726b-4dee-b026-622b48a61099"),
+                    Street = "Street 2",
+                    HouseNumber = "2",
+                    HouseNumberExtension = "B",
+                    HouseNumberExtensionExtra = "C",
+                    ZipCode = "4567AB",
+                    City = "Amsterdam",
+                    Province = "Noord-Holland",
+                    CountryCode = "NL"
+                }
+            };
+        }
+
+        private static List<Warehouse>GetWarehouses()
+        {
+            return new List<Warehouse>()
+            {
+                new Warehouse(newInstance: true)
+                {
+                    Id = Guid.Parse("78b5c277-8784-4eb6-ac7d-a1f07dab6e49"),
+                    Code = "123",
+                    Name = "Amstelveen",
+                    AddressId = Guid.Parse("cefc9e60-7d37-41f5-b3c8-3144894f207e"),
+                }
+            };
+
+            
+        }
+        
     }
 }

bc19687d7f9a05bc69f3da3ffd8d0c0fb9f99971
sidi <sidimo2001@gmail.com>
2025-01-13 15:24:30 +0100

    fix(Shipments): response messege

--- api/Controllers/ShipmentsController.cs
+++ api/Controllers/ShipmentsController.cs
@@ -19,30 +19,34 @@ public class ShipmentsController : ControllerBase
         if (newShipment == null) throw new ApiFlowException("Saving new Shipment failed.");
 
 
-        return Ok(new ShipmentResponse
+        return Ok(new
         {
-            Id = newShipment.Id,
-            OrderId = newShipment.OrderId,
-            OrderDate = newShipment.OrderDate,
-            RequestDate = newShipment.RequestDate,
-            ShipmentDate = newShipment.ShipmentDate,
-            ShipmentType = newShipment.ShipmentType.ToString(),
-            ShipmentStatus = newShipment.ShipmentStatus.ToString(),
-            Notes = newShipment.Notes,
-            CarrierCode = newShipment.CarrierCode,
-            CarrierDescription = newShipment.CarrierDescription,
-            ServiceCode = newShipment.ServiceCode,
-            PaymentType = newShipment.PaymentType.ToString(),
-            TransferMode = newShipment.TransferMode.ToString(),
-            TotalPackageCount = newShipment.TotalPackageCount,
-            TotalPackageWeight = newShipment.TotalPackageWeight,
-            CreatedAt = newShipment.CreatedAt,
-            UpdatedAt = newShipment.UpdatedAt,
-            Items = newShipment.ShipmentItems?.Select(si => new ShipmentItemRR
+            message = "Shipment created!",
+            Shipment = new ShipmentResponse
             {
-                ItemId = si.ItemId,
-                Amount = si.Amount
-            }).ToList()
+                Id = newShipment.Id,
+                OrderId = newShipment.OrderId,
+                OrderDate = newShipment.OrderDate,
+                RequestDate = newShipment.RequestDate,
+                ShipmentDate = newShipment.ShipmentDate,
+                ShipmentType = newShipment.ShipmentType.ToString(),
+                ShipmentStatus = newShipment.ShipmentStatus.ToString(),
+                Notes = newShipment.Notes,
+                CarrierCode = newShipment.CarrierCode,
+                CarrierDescription = newShipment.CarrierDescription,
+                ServiceCode = newShipment.ServiceCode,
+                PaymentType = newShipment.PaymentType.ToString(),
+                TransferMode = newShipment.TransferMode.ToString(),
+                TotalPackageCount = newShipment.TotalPackageCount,
+                TotalPackageWeight = newShipment.TotalPackageWeight,
+                CreatedAt = newShipment.CreatedAt,
+                UpdatedAt = newShipment.UpdatedAt,
+                Items = newShipment.ShipmentItems?.Select(si => new ShipmentItemRR
+                {
+                    ItemId = si.ItemId,
+                    Amount = si.Amount
+                }).ToList()
+            }
         });
     }
 
@@ -53,10 +57,10 @@ public class ShipmentsController : ControllerBase
 
         return updatedShipment == null
             ? NotFound(new { message = $"Shipment not found for id '{id}'" })
-            : Ok(new
+            :  Ok(new
             {
                 message = "Shipment updated!",
-                updated_shipment = new ShipmentResponse
+                Shipment = new ShipmentResponse
                 {
                     Id = updatedShipment.Id,
                     OrderId = updatedShipment.OrderId,
@@ -155,30 +159,34 @@ public class ShipmentsController : ControllerBase
 
         return (foundShipment == null)
             ? NotFound(new { message = $"Shipment not found for id '{id}'" })
-            : Ok(new ShipmentResponse
+            :  Ok(new
             {
-                Id = foundShipment.Id,
-                OrderId = foundShipment.OrderId,
-                OrderDate = foundShipment.OrderDate,
-                RequestDate = foundShipment.RequestDate,
-                ShipmentDate = foundShipment.ShipmentDate,
-                ShipmentType = foundShipment.ShipmentType.ToString(),
-                ShipmentStatus = foundShipment.ShipmentStatus.ToString(),
-                Notes = foundShipment.Notes,
-                CarrierCode = foundShipment.CarrierCode,
-                CarrierDescription = foundShipment.CarrierDescription,
-                ServiceCode = foundShipment.ServiceCode,
-                PaymentType = foundShipment.PaymentType.ToString(),
-                TransferMode = foundShipment.TransferMode.ToString(),
-                TotalPackageCount = foundShipment.TotalPackageCount,
-                TotalPackageWeight = foundShipment.TotalPackageWeight,
-                CreatedAt = foundShipment.CreatedAt,
-                UpdatedAt = foundShipment.UpdatedAt,
-                Items = foundShipment.ShipmentItems?.Select(item => new ShipmentItemRR
+                message = "Shipment found!",
+                Shipment = new ShipmentResponse
                 {
-                    ItemId = item.ItemId,
-                    Amount = item.Amount
-                }).ToList()
+                    Id = foundShipment.Id,
+                    OrderId = foundShipment.OrderId,
+                    OrderDate = foundShipment.OrderDate,
+                    RequestDate = foundShipment.RequestDate,
+                    ShipmentDate = foundShipment.ShipmentDate,
+                    ShipmentType = foundShipment.ShipmentType.ToString(),
+                    ShipmentStatus = foundShipment.ShipmentStatus.ToString(),
+                    Notes = foundShipment.Notes,
+                    CarrierCode = foundShipment.CarrierCode,
+                    CarrierDescription = foundShipment.CarrierDescription,
+                    ServiceCode = foundShipment.ServiceCode,
+                    PaymentType = foundShipment.PaymentType.ToString(),
+                    TransferMode = foundShipment.TransferMode.ToString(),
+                    TotalPackageCount = foundShipment.TotalPackageCount,
+                    TotalPackageWeight = foundShipment.TotalPackageWeight,
+                    CreatedAt = foundShipment.CreatedAt,
+                    UpdatedAt = foundShipment.UpdatedAt,
+                    Items = foundShipment.ShipmentItems?.Select(item => new ShipmentItemRR
+                    {
+                        ItemId = item.ItemId,
+                        Amount = item.Amount
+                    }).ToList()
+                }
             });
     }
 }

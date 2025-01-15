1be390e131c14e62e23566a80cceaed027d1984f
Ahmet <38209407+ahmetDev7@users.noreply.github.com>
2025-01-15 08:58:19 +0100

    fix(integration tests): Fixed API keys for integration tests

--- api.IntegrationTests/ItemIntegrationTests.cs
+++ api.IntegrationTests/ItemIntegrationTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -9,6 +10,7 @@ namespace api.IntegrationTests
     {
         private readonly HttpClient _httpClient;
         private readonly string _baseUrl = "/api/Items";
+        private readonly string adminKey = ApiKeyLoader.LoadApiKeyFromJson(".env.json", "API_ADMIN");
 
         public ItemIntegrationTests(CustomWebApplicationFactory<Program> factory)
         {
@@ -16,6 +18,8 @@ namespace api.IntegrationTests
             {
                 AllowAutoRedirect = false
             });
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", adminKey);
         }
 
         [Fact]
--- api.IntegrationTests/WarehousesIntegrationTests.cs
+++ api.IntegrationTests/WarehousesIntegrationTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -10,12 +11,16 @@ namespace api.IntegrationTests
         private readonly HttpClient _httpClient;
         private readonly string _baseUrl = "/api/Warehouses";
 
+        private readonly string adminKey = ApiKeyLoader.LoadApiKeyFromJson(".env.json", "API_ADMIN");
+
         public WarehousesIntegrationTests(CustomWebApplicationFactory<Program> factory)
         {
             _httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions
             {
                 AllowAutoRedirect = false
             });
+            
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", adminKey);
         }
 
         [Fact]

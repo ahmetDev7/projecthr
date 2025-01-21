4f229944ffbb493fa57e14f86e984a8b4790a41f
Sharif-C <sharifali.786@hotmail.com>
2025-01-21 14:17:16 +0100

    test(Address): implement integration test

--- api.IntegrationTests/AddressesIntegrationTests.cs
+++ api.IntegrationTests/AddressesIntegrationTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using DTO.Address;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 
@@ -45,5 +46,55 @@ namespace api.IntegrationTests
 
             result.Should().NotBeNull();
         }
+
+        [Fact]
+        public async Task CreateAddress_CreatesNewAddress()
+        {
+            var body = new AddressRequest()
+            {
+                Street = "Keizersgracht",
+                HouseNumber = "456",
+                HouseNumberExtension = "B",
+                HouseNumberExtensionExtra = "Side Entrance",
+                ZipCode = "1015 CX",
+                City = "Amsterdam",
+                Province = "North Holland",
+                CountryCode = "NL"
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(_baseUrl, body);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task UpdateAddress_UpdateAddress()
+        {
+            var updateResponse = await _httpClient.PutAsJsonAsync(_baseUrl + "/cefc9e60-7d37-41f5-b3c8-3144894f207e", new AddressRequest()
+            {
+                Street = "Oranjestraat",
+                HouseNumber = "456",
+                HouseNumberExtension = "B",
+                HouseNumberExtensionExtra = "Side Entrance",
+                ZipCode = "1015 CX",
+                City = "Amsterdam",
+                Province = "North Holland",
+                CountryCode = "NL"
+            });
+            updateResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task DeleteAddress_DeleteExistingAddress_CheckIfAddress()
+        {
+            var deleteResponse = await _httpClient.DeleteAsync(_baseUrl + "/" + "2ff2c789-726b-4dee-b026-622b48a61099");
+
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var getResponse = await _httpClient.GetAsync(_baseUrl + "/" + "2ff2c789-726b-4dee-b026-622b48a61099");
+
+            getResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
     }
 }
